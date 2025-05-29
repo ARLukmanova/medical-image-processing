efc4b5e2823b029e4d55c200f3693fe9056b39cb
Alina Lukmanova <alina@Sergey23.integro.local>
2025-05-30 01:22:22 +0500

    add HTTP response codes

--- classification-service/main.py
+++ classification-service/main.py
@@ -20,8 +20,8 @@ async def predict_endpoint(file: UploadFile = File(...)) -> JSONResponse:
         task = celery_app.send_task("predict_task.predict_task", args=[file_bytes, file.filename])
         return JSONResponse(content={
             "task_id": task.id,
-            "status": "processing"
-        })
+            "status": "processing",
+        }, status_code=202)
     except HTTPException:
         raise
     except Exception as e:
@@ -33,14 +33,14 @@ async def predict_endpoint(file: UploadFile = File(...)) -> JSONResponse:
 async def get_prediction_status(task_id: str) -> JSONResponse:
     task = celery_app.AsyncResult(task_id)
     if task.state == "PENDING":
-        result =  {"status": "processing"}
+        result =  {"status": "processing", "status_code": 202}
     elif task.state == "SUCCESS":
-        result = {"status": "done", "result": task.result}
+        result = {"status": "done", "result": task.result, "status_code": 200}
     elif task.state == "FAILURE":
-        result =  {"status": "error", "error": str(task.info)}
+        result =  {"status": "error", "error": str(task.info), "status_code": 500}
     else:
         result =  {"status": task.state}
-    return JSONResponse(content=result)
+    return JSONResponse(content=result, status_code=result["status_code"])
 
 # @app.get("/update_model_version")
 # async def update_model_version_endpoint() -> JSONResponse:
