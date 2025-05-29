f27ffcb8a81e877a2cef1c98dfb941513cf307b3
Alina Lukmanova <alina@Sergey23.integro.local>
2025-05-29 23:24:28 +0500

    move prediction to celery worker; adjust docker-compose.yml

--- classification-service/main.py
+++ classification-service/main.py
@@ -4,16 +4,10 @@ import uvicorn
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
 
+from celery_app import celery_app
 from logger import logger
-from model import Model
-from model_registry_tools import ensure_model_file_exists, init_mlflow, download_new_model_version
-from parameters import CLASS_NAMES
-from plot_tools import create_probability_plot
 
 app = FastAPI(title="X-Ray Classification API")
-init_mlflow()
-ensure_model_file_exists()
-model = Model()
 
 
 @app.post("/predict")
@@ -23,30 +17,11 @@ async def predict_endpoint(file: UploadFile = File(...)) -> JSONResponse:
         logger.info(f"Получен запрос на классификацию от {file.filename}")
 
         file_bytes = await file.read()
-        pred = model.predict(file_bytes)
-        plot_base64 = create_probability_plot(pred['probabilities'])
-
-        # Формируем детализированный ответ
-        pneumonia_prob = pred['probabilities'][1]  # Вероятность пневмонии
-        is_pneumonia = pred['prediction'] == 1
+        task = celery_app.send_task("predict_task.predict_task", args=[file_bytes, file.filename])
         return JSONResponse(content={
-            "status": "success",
-            "filename": file.filename,
-            "prediction": {
-                "class": CLASS_NAMES[pred['prediction']],
-                "probability": float(pred['probability']),
-                "is_pneumonia": bool(is_pneumonia),
-                "pneumonia_probability": float(pneumonia_prob),
-                "normal_probability": float(pred['probabilities'][0]),
-                "message": "Анализ выполнен гибридной моделью (CNN + ViT)"
-            },
-            "plot_image": plot_base64,
-            "recommendation": {
-                "action": "Рекомендуется консультация врача" if is_pneumonia else "Патологий не обнаружено",
-                "urgency": "Срочно" if pneumonia_prob > 0.7 else "Планово" if pneumonia_prob > 0.3 else "Не требуется"
-            }
+            "task_id": task.id,
+            "status": "processing"
         })
-
     except HTTPException:
         raise
     except Exception as e:
@@ -54,19 +29,32 @@ async def predict_endpoint(file: UploadFile = File(...)) -> JSONResponse:
         raise HTTPException(status_code=500, detail="Внутренняя ошибка сервера")
 
 
-@app.get("/update_model_version")
-async def update_model_version_endpoint() -> JSONResponse:
-    try:
-        logger.info("Запрос на обновление версии модели")
-        download_new_model_version()
-        model.load_model()
-        return JSONResponse(content={
-            "status": "success",
-            "message": "Модель успешно обновлена"
-        })
-    except Exception as e:
-        logger.error(f"Ошибка при обновлении модели: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="Ошибка при обновлении модели")
+@app.get("/predict_status/{task_id}")
+async def get_prediction_status(task_id: str) -> JSONResponse:
+    task = celery_app.AsyncResult(task_id)
+    if task.state == "PENDING":
+        result =  {"status": "processing"}
+    elif task.state == "SUCCESS":
+        result = {"status": "done", "result": task.result}
+    elif task.state == "FAILURE":
+        result =  {"status": "error", "error": str(task.info)}
+    else:
+        result =  {"status": task.state}
+    return JSONResponse(content=result)
+
+# @app.get("/update_model_version")
+# async def update_model_version_endpoint() -> JSONResponse:
+#     try:
+#         logger.info("Запрос на обновление версии модели")
+#         download_new_model_version()
+#         model.load_model()
+#         return JSONResponse(content={
+#             "status": "success",
+#             "message": "Модель успешно обновлена"
+#         })
+#     except Exception as e:
+#         logger.error(f"Ошибка при обновлении модели: {e}", exc_info=True)
+#         raise HTTPException(status_code=500, detail="Ошибка при обновлении модели")
 
 
 if __name__ == "__main__":
---
+++ classification-service/predict_task.py
@@ -0,0 +1,34 @@
+from celery_app import celery_app
+from model import Model
+from model_registry_tools import init_mlflow, ensure_model_file_exists
+from parameters import CLASS_NAMES
+from plot_tools import create_probability_plot
+
+init_mlflow()
+ensure_model_file_exists()
+model = Model()
+
+@celery_app.task
+def predict_task(image_bytes, filename):
+    pred = model.predict(image_bytes)
+    plot_base64 = create_probability_plot(pred['probabilities'])
+
+    pneumonia_prob = pred['probabilities'][1]  # Вероятность пневмонии
+    is_pneumonia = pred['prediction'] == 1
+    return {
+        "status": "success",
+        "filename": filename,
+        "prediction": {
+            "class": CLASS_NAMES[pred['prediction']],
+            "probability": float(pred['probability']),
+            "is_pneumonia": bool(is_pneumonia),
+            "pneumonia_probability": float(pneumonia_prob),
+            "normal_probability": float(pred['probabilities'][0]),
+            "message": "Анализ выполнен гибридной моделью (CNN + ViT)"
+        },
+        "plot_image": plot_base64,
+        "recommendation": {
+            "action": "Рекомендуется консультация врача" if is_pneumonia else "Патологий не обнаружено",
+            "urgency": "Срочно" if pneumonia_prob > 0.7 else "Планово" if pneumonia_prob > 0.3 else "Не требуется"
+        }
+    }
